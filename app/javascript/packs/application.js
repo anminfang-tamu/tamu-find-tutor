315675e605eeb48e701c181c4dce01ecca281018
Ubuntu <ubuntu@ip-172-31-26-65.ec2.internal>
2022-03-22 02:52:48 +0000

    Commented turbolink out

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,10 +20,10 @@
 import 'bootstrap';
 import '../stylesheets/application'
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
 
 require("@popperjs/core")
 
