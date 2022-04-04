0fe0f44e115f4df4f18bd5d60c06ac2ee60843c9
Ubuntu <ubuntu@ip-172-31-26-65.ec2.internal>
2022-04-04 04:24:20 +0000

    Added img tag on user template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
         hash = Digest::MD5.hexdigest(email_address)
         size = options[:size]
         image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
-        image_tag(image_src, alt: user.username, class: "rounde shadow mx-auto d-block")
+        image_tag(image_src, alt: user.username, class: "rounde shadow mx-auto d-block mt-2")
     end
     
 end
