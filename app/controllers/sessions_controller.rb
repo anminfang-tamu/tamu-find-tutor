5b7525fd2db3b5de28f06367e5cded368526ddca
Ubuntu <ubuntu@ip-172-31-26-65.ec2.internal>
2022-04-06 05:02:57 +0000

    Added login and logout func

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,20 @@ class SessionsController < ApplicationController
     
     def create
         user = User.find_by(user_email: params[:session][:email].downcase)
-        p user
         if user && user.authenticate(params[:session][:password])
-            flash[:notice] = "Logged in successfully."
-            redirect_to root_path
+            session[:user_id] = user.id
+            flash[:notice] = "Logged in successfully"
+            redirect_to posts_path
         else
-            flash.now[:alert] = "There was something wrong with your login details."
+            flash.now[:alert] = "There was something wrong with your login details"
             render 'new'
         end
     end
     
     def destroy
+        session[:user_id] = nil
+        flash[:notice] = "Logged out"
+        redirect_to root_path
     end
 
 end
