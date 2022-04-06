0b602eb67dcb794915e6a164016bd1c44475b017
Ubuntu <ubuntu@ip-172-31-26-65.ec2.internal>
2022-04-06 04:46:03 +0000

    Added shared view and fixed password digest bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
     end
     
     def create
+        user = User.find_by(user_email: params[:session][:email].downcase)
+        p user
+        if user && user.authenticate(params[:session][:password])
+            flash[:notice] = "Logged in successfully."
+            redirect_to root_path
+        else
+            flash.now[:alert] = "There was something wrong with your login details."
+            render 'new'
+        end
     end
     
     def destroy
