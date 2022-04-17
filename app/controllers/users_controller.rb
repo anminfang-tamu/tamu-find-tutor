399f34f4f494b1186217974480ec4f671f35518e
Ubuntu <ubuntu@ip-172-31-26-65.ec2.internal>
2022-04-17 04:02:27 +0000

    Added restrict func to prevent other users to edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]
     skip_before_action :verify_authenticity_token
     
     def show
@@ -51,6 +52,13 @@ class UsersController < ApplicationController
     
     def user_params
         params.require(:user).permit(:username, :fullname, :user_email, :password)
+    end
+    
+    def require_same_user
+        if current_user != @user
+            flash[:alert] = "You can only edit your own account"
+            redirect_to @user
+        end
     end
     
 end
