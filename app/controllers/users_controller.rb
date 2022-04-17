18f4775d8250c292a2d02d1db6bbce6b91482ae9
Ubuntu <ubuntu@ip-172-31-26-65.ec2.internal>
2022-04-17 03:47:19 +0000

    Restricted user edit without login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, only: [:edit, :update]
     skip_before_action :verify_authenticity_token
     
     def show
