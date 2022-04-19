48ba764fe68cc5ec7dc0720c9c817bdde6b95d27
Ubuntu <ubuntu@ip-172-31-26-65.ec2.internal>
2022-04-19 04:14:55 +0000

    Modified message alert popup func and bootstrap setting

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,10 +2,17 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require("webpack")
 
+// environment.plugins.append("Provide", new webpack.ProvidePlugin({
+//     $: 'jquery/src/jquery',
+//     jQuery: 'jquery/src/jquery',
+//     Popper: ['popper.js', 'default']  // Not a typo, we're still using popper.js here
+// }))
+
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery',
-    Popper: ['popper.js', 'default']  // Not a typo, we're still using popper.js here
-}))
+    $: 'jquery',
+    jQuery: 'jquery',
+    'window.jQuery': 'jquery',
+    Popper: ['popper.js', 'default']
+  }))
 
 module.exports = environment
