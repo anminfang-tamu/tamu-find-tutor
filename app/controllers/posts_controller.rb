6c04e6568badef4fac225b56885f3ec744da8c59
Ubuntu <ubuntu@ip-172-31-26-65.ec2.internal>
2022-04-24 03:59:45 +0000

    Added search view and func in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :require_user, except: [:show, :index]
+  before_action :require_user, except: [:show, :index, :search]
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
   # GET /posts
@@ -45,6 +45,10 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
+  
+  def search
+    
   end
 
   private
